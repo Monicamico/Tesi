b2541ce557159dbab9c72d9223088345726aadb6
Monica Amico <Monicat@Air-di-Monica.lan>
2020-05-25 11:39:08 +0200

    aggiunti error handler

--- dashboard/home.py
+++ dashboard/home.py
@@ -1,20 +1,29 @@
-from flask import Blueprint, render_template, request as http_req
-from flask import json
-from gio_db import ConnectionRequest, Plant, Radio
+from flask import Blueprint, render_template
+from gio_db import ConnectionRequest
 
 homepage = Blueprint('homepage', __name__)
 
 
-@homepage.route('/')
+@homepage.route('/', methods=['GET', 'POST'])
 def layout_page():
     conn_list = ConnectionRequest.query.all()
     return render_template('home.html',
                            connections=conn_list)
 
 
-@homepage.route('/home')
+@homepage.route('/home', methods=['GET', 'POST'])
 def home_page():
     conn_list = ConnectionRequest.query.all()
     return render_template('home.html',
                            connections=conn_list)
+
+
+@homepage.app_errorhandler(404)
+def not_found_page(error):
+    return render_template('error.html', error='404')
+
+
+@homepage.app_errorhandler(401)
+def permission_denied_page(error):
+    return render_template('error.html', error='401')
 
