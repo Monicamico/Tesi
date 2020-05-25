190b5e527704f5f3a6e68af19582d1a807c5b009
Monica Amico <Monicat@Air-di-Monica.lan>
2020-05-25 11:39:26 +0200

    file autenticazione login

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
-from constant import DEBUG
+from auth import login_manager
+from auth import auth
+from constant import Role, DEBUG
 from dashboard import dashboard_page
 from home import homepage
 from connections import connections_page
@@ -10,8 +11,9 @@ from radios import radios_page
 from map import map_page
 from radio_settings import radio_settings_page
 from plant_settings import settings_page
-from gio_db import db, add_conn_req, delete_conn_req, delete_plant, add_radio, add_plant, update_vase_state
+from gio_db import db, add_user, User, add_plant, add_radio, update_light, update_hum, update_temp
 from request import request_page
+from users import users_page
 
 
 def create_app():
@@ -19,96 +21,42 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # suppress pytest warning
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///giovase.db'
     app.config['SECRET_KEY'] = 'keysecret0679623'
+
+    app.register_blueprint(homepage)
     app.register_blueprint(plants_page)
     app.register_blueprint(radios_page)
     app.register_blueprint(plant_id_page)
-    app.register_blueprint(homepage)
     app.register_blueprint(connections_page)
     app.register_blueprint(settings_page)
     app.register_blueprint(radio_settings_page)
     app.register_blueprint(map_page)
     app.register_blueprint(dashboard_page)
     app.register_blueprint(request_page)
+    app.register_blueprint(users_page)
+    app.register_blueprint(auth)
+
+    login_manager.init_app(app)
 
     db.init_app(app=app)
-
-
-    """
-     if DEBUG:
-        db.drop_all(app=app)
-         with app.app_context():
-        add_radio(1, 'urlradio1')
-        add_radio(2, 'urlradio2')
-        add_radio(3, 'urlradio3')
-        add_radio(4, 'urlradio4')
-        add_radio(5, 'urlradio5')
-        add_radio(6, 'urlradio6')
-        add_conn_req(10, 11, 345, 'urlradio1')
-        add_plant(10, 1, 1)
-        add_conn_req(11, 11, 345, 'urlradio1')
-        add_plant(11, 1, 1)
-        add_conn_req(12, 12, 432, 'urlradio2')
-        add_plant(12, 1, 1)
-        add_conn_req(13, 11, 345, 'urlradio1')
-        add_plant(13, 11, 1)
-        add_conn_req(14, 11, 345, 'urlradio1')
-        add_plant(14, 1, 2)
-        add_conn_req(15, 11, 345, 'urlradio1')
-        add_plant(15, 1, 3)
-        add_conn_req(16, 11, 345, 'urlradio2')
-        add_plant(16, 1, 3)
-        add_conn_req(17, 11, 345, 'urlradio2')
-        add_plant(17, 1, 3)
-        add_conn_req(18, 11, 345, 'urlradio1')
-        add_plant(18, 1, 3)
-
-        add_conn_req(31, 11, 345, 'urlradio1')
-        add_plant(31, 1, 5)
-        add_conn_req(32, 11, 345, 'urlradio1')
-        add_plant(32, 1, 5)
-        add_conn_req(19, 11, 345, 'urlradio1')
-        add_plant(19, 1, 5)
-
-        add_conn_req(21, 11, 345, 'urlradio1')
-        add_plant(21, 1, 5)
-        add_conn_req(22, 11, 345, 'urlradio1')
-        add_plant(22, 1, 5)
-        add_conn_req(23, 11, 345, 'urlradio1')
-        add_plant(23, 4, 4)
-        add_conn_req(24, 11, 345, 'urlradio1')
-        add_plant(24, 1, 6)
-        add_conn_req(25, 11, 345, 'urlradio1')
-        add_plant(25, 1, 6)
-        add_conn_req(26, 11, 345, 'urlradio1')
-        add_plant(26, 4, 6)
-        add_conn_req(27, 11, 345, 'urlradio1')
-        add_plant(27, 1, 4)
-        add_conn_req(28, 11, 345, 'urlradio1')
-        add_plant(28, 1, 6)
-        add_conn_req(29, 11, 345, 'urlradio1')
-        add_plant(29, 1, 6)
-        update_vase_state(25, 1, 0)
-        update_vase_state(22, 1, 0)
-        update_vase_state(24, 1, 0)
-        update_vase_state(13, 1, 0)
-        update_vase_state(15, 1, 0)
-        update_vase_state(14, 1, 0)
-        add_conn_req(34, 11, 345, 'urlradio1')
-        add_plant(34, 1, 2)
-        add_conn_req(35, 11, 345, 'urlradio1')
-        add_plant(35, 1, 2)
-        add_conn_req(36, 11, 345, 'urlradio1')
-        add_plant(36, 1, 2)
-        
-        add_conn_req(20, 11, 345, 'urlradio1')
-        add_plant(20, 1, 5)
-
-    """
     db.create_all(app=app)
+    with app.app_context():
+        if User.query.filter_by(username="admin").first() is None:
+            add_user("admin", "admin", Role.ADMIN.value)
+        if User.query.filter_by(username="Monica").first() is None:
+            add_user("Monica", "utente23", Role.ADMIN.value)
+        if User.query.filter_by(username="Utente").first() is None:
+            add_user("Utente", "utente", Role.USER.value)
+        """
+        add_radio(-417538279765,"urlradio1")
+        add_plant("Kalanchoe",850823,-417538279765)
+        update_light("Orchidee",4322,30)
+        update_hum("Orchidee",4322,267)
+        update_temp("Orchidee",4332,8)
+        """
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
 
---
+++ dashboard/auth.py
@@ -0,0 +1,51 @@
+from flask_login import login_manager, current_user, LoginManager, login_required, logout_user, login_user
+from gio_db import User, get_user
+from flask import Blueprint, redirect, flash, render_template, request as rcv_req
+from forms import LoginForm
+
+login_manager = LoginManager()
+
+auth = Blueprint('auth', __name__)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+def is_admin():
+    if current_user.is_authenticated and current_user.is_admin:
+        return True
+    else:
+        return login_manager.unauthorized()
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if hasattr(current_user, "is_authenticated") and current_user.is_authenticated:
+        return redirect("/home")
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = rcv_req.form['username']
+        pwd = rcv_req.form['password']
+        user = get_user(name)
+        if user is not None and user.authenticate(password=pwd):
+            if login_user(user):
+                flash('Login effettuato con succeesso','success')
+                return redirect('/home')
+            else:
+                flash("Impossibile effettuare il login",'error')
+                return redirect('/login')
+        else:
+            flash("Username o password errati")
+            return redirect("/login")
+    return render_template('login.html', form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
