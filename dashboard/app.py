d021f30351bb52e89ffb38748e378af65443f4ee
Monica Amico <Monicat@host-131-114-212-70.m.unipi.it>
2020-09-17 15:31:39 +0200

    cambiati indirizzi IP e tolto il pacchetto pytz

--- dashboard/app.py
+++ dashboard/app.py
@@ -60,4 +60,4 @@ if __name__ == "__main__":
     with app.app_context():
         add_user('Admin', 'admin', 0)
         add_user('Monica', 'utente23', 1)
-    app.run(host='127.0.0.1',port=5000)
+    app.run(host='0.0.0.0',port=5000)
