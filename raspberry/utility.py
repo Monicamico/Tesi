3043fc1a47d6d0a9d44dc23ef94675199f400156
Monica Amico <Monicat@host-131-114-212-170.m.unipi.it>
2020-09-18 10:39:21 +0200

    Sistemata funzione per l'ottenimento dell'ip

--- raspberry/utility.py
+++ raspberry/utility.py
@@ -3,9 +3,17 @@ import threading
 
 
 def get_ip():
-    hostname = socket.gethostname()
-    ip = socket.gethostbyname(hostname)
-    return ip
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        hostname = socket.gethostname()
+        IP = socket.gethostbyname(hostname)
+    finally:
+        s.close()
+    return IP
 
 
 # lock for serial queue
